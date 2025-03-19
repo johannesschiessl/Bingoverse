7d425a6319741f7a91a3b1f7e2f677b9f8b892c6
Johannes Schießl <johannes@ignotum.dev>
2025-03-19 10:47:02 +0100

    feat(components): rename to Eventverse

--- src/components/app/layout/header.tsx
+++ src/components/app/layout/header.tsx
@@ -7,7 +7,7 @@ export default function Header() {
     <header className="p-4 flex items-center justify-between">
       <Link href="/" className="flex items-center gap-2">
         <LayoutGrid className="size-8 p-1 bg-orange-100 text-orange-500 rounded-md" />
-        <span className="text-xl font-bold">Bingoverse</span>
+        <span className="text-xl font-bold">Eventverse</span>
       </Link>
       <Link href="/new">
         <Button variant="outline">
